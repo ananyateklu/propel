21b8cb0513731d684d71123a61e1bb69e9e21fcd
ananyateklu <teklu009@morris.umn.edu>
2019-12-16 11:55:32 -0600

    Add the numbers 0 through 9 and the function integer_combine into our instruction list

--- src/propel/core.clj
+++ src/propel/core.clj
@@ -14,6 +14,17 @@
   (list
    'integer_+
    'integer_-
+   'integer_combine
+   0
+   1
+   2
+   3
+   4
+   5
+   6
+   7
+   8
+   9
    ))
 
 (def opens ; number of blocks opened by instructions (default = 0)
